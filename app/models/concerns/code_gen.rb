6fabe080cb29429763d2231a3796b1dd1f301f8e
mgiacomi <Rebeka13!>
2017-03-23 22:34:47 -0700

    fix the small stuff.

--- app/models/concerns/code_gen.rb
+++ app/models/concerns/code_gen.rb
@@ -3,16 +3,9 @@ module CodeGen
 
   included do
     before_create :gen_code
-    #after_create :gen_bitly
   end
 
   module ClassMethods
-  end
-
-  def gen_bitly
-    pub_profile_url = Rails.application.routes.url_helpers.pub_profile_url(host: 'www.read4richmond.com', id: obscure_id)
-    bitly_pub_profile_url = Bitly.client.shorten(pub_profile_url).short_url
-    self.update_attributes(bitly_profile: bitly_pub_profile_url, bitly_pledge: bitly_pub_pledge_url)
   end
 
   private
